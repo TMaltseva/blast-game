380bec5e516de640d288aafeba960698fcd94a5e
TMaltseva <bacardeonie@gmail.com>
2026-03-26 07:19:39 -0700

    fix: fix resolution

--- src/settings.12fa0.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/blast.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"e6a83",main:"2104c"}};
